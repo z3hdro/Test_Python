24b68e2fc95e320ecaca6974bdce5d967b234aac
z3hdro <z3hdro@8e86bfb0e784>
2020-06-19 14:17:04 +0000

    some fixes with error403

--- marvelapi/views.py
+++ marvelapi/views.py
@@ -6,11 +6,14 @@ import hashlib
 import time
 from marvelapi.models import Rus
 from marvelapi.config import private_key, public_key
+from rest_framework.decorators import api_view
 
 
 ts = str(time.time())
 m = hashlib.md5((ts+private_key+public_key).encode('utf-8')).hexdigest()
 
+
+@api_view(['GET'])
 def show_films(request):
     response = requests.get(f'http://gateway.marvel.com/v1/public/comics?ts={ts}&apikey={public_key}&hash={m}')
     result = response.json()
@@ -25,5 +28,7 @@ def show_films(request):
             data[i] = (data[i], 'Rus_name does not exist')
     return JsonResponse({'result': data}, json_dumps_params={'ensure_ascii':False})
 
+
+@api_view(['GET'])
 def local_comic_names(request):
     return JsonResponse({'result': list(Rus.objects.values('eng_title','rus_title'))}, json_dumps_params={'ensure_ascii':False})
